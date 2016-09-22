063d73ce2e8ab7dce26f81f6a82b5ff6c61cf520
Sebastian Kusnier <sebastian@kusnier.net>
2016-09-22 13:02:01 +0200

    Add support for jboss7, wildfly9 and wildfly10

--- src/test/java/org/arquillian/example/helloworld/GreetingServiceTestCase.java
+++ src/test/java/org/arquillian/example/helloworld/GreetingServiceTestCase.java
@@ -1,10 +1,12 @@
 package org.arquillian.example.helloworld;
 
+
 import javax.inject.Inject;
 
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,7 +18,8 @@ public class GreetingServiceTestCase {
 	@Deployment
 	public static WebArchive deployService() {
 		return ShrinkWrap.create(WebArchive.class)
-				.addClass(GreetingService.class);
+				.addClass(GreetingService.class)
+                .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml"); // jboss 7.1.1 - jee6
 	}
 
 	@Inject
